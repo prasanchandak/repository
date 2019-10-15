c67f178b4eaa974a1a99bab95194d77b7d2532dd
dadhichvivek <56474890+dadhichvivek@users.noreply.github.com>
2019-10-15 23:52:25 +0530

    Create working with zoo.py

---
+++ working with zoo.py
@@ -0,0 +1,8 @@
+import pandas as pd
+data=pd.read_csv('zoo.csv', delimiter = ',')
+data['water_need']
+list=[]
+pd.read_csv('pandas_tutorial_read.csv', delimiter=';')
+pd.read_csv('pandas_tutorial_read.csv', delimiter=';', names = ['my_datetime', 'event', 'country', 'user_id', 'source', 'topic'])
+article_read.head()
+article_read = pd.read_csv('pandas_tutorial_read.csv', delimiter=';', names = ['my_datetime', 'event', 'country', 'user_id', 'source', 'topic'])

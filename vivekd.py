7ee145994fde59e63fe61bed3d09811b79f6437e
dadhichvivek <56474890+dadhichvivek@users.noreply.github.com>
2019-10-15 23:40:12 +0530

    Create vivekd.py

---
+++ vivekd.py
@@ -0,0 +1,9 @@
+a=int(input("give me a number:"))
+b=int(input("give me a number:"))
+c=int(input("give me a number:"))
+n=(int(a)),(int(b)),(int(c))
+m=((int(a))**3+(int(b))**3+(int(c))**3)
+if (n==m):
+    print("satisfied")
+else:
+    print("not satisfied")
